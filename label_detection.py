0d18c5403ff6121faae07fa2811a48bb675df50a
Neal Satitsumpun <satitsumpun@wisc.edu>
2019-04-22 23:16:43 -0500

    add text detection code from google cloud vision

---
+++ label_detection.py
@@ -0,0 +1,26 @@
+import io
+
+def detect_text(path):
+    """Detects text in the file."""
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+        print(type(content))
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    # print('Texts:')
+
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+detect_text('examples/if-you-think-were-bad-try-looking-in-the-mirror.jpg')
